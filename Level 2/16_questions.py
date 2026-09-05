00182094bce0fa9b4173934a12f9a442485c13d1
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-05 13:56:41 +0530

    Create 16_questions.py

---
+++ Level 2/16_questions.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a Number: "))
+count = 0
+for i in range(1, a + 1):
+    if a % i == 0:
+        count = count + 1
+if count == 2:
+    print("Prime")
+else:
+    print("Not Prime")
+  
