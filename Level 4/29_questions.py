4645a3f5ceca8ca102b9442bf70243c6ec808c8b
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-06 13:05:43 +0530

    Create 29_questions.py

---
+++ Level 4/29_questions.py
@@ -0,0 +1,9 @@
+for i in range(9999, 999, -1):
+    count = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            count = count + 1
+    if count == 2:
+        print("Output: ", i)
+        break
+      
