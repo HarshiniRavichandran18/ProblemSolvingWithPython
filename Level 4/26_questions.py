fbf04efe58fa7c90756183502edfb8fbe878aa5f
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-06 13:03:46 +0530

    Create 26_questions.py

---
+++ Level 4/26_questions.py
@@ -0,0 +1,9 @@
+for i in range(100, 1000):
+    count = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            count = count + 1
+    if count == 2:
+        print("Output: ", i)
+        break
+      
