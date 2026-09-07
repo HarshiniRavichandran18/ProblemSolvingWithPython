6fe90669d979e9d0ed23dc7be190a91c52f2106a
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:21:15 +0530

    Update 34_questions.py

--- Level 1/34_questions.py
+++ Level 1/34_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get two 3-digit numbers from user. Print the difference between the one's digit and 
+hundred's digit of the number whose ten's digit is bigger than the other number's ten's digit. 
+Testcase: 
+Input: 856, 978 → Output: 1 
+Input: 128, 365 → Output: 2'''
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 def check(a, b):
