eaacf9e310304f527e62ff01592ea50507baa6d6
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:45:54 +0530

    Update 04_questions.py

--- Level 3/04_questions.py
+++ Level 3/04_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a number from user and check whether it is prime or not, then print the 
+result. 
+Testcase: 
+Input: 61 → Output: Number is Prime 
+Input: 1200 → Output: Number is not Prime'''
+
 def check(a):
     count = 0
     for i in range(1, a + 1):
