ec1515ea4e4df9cfe8008271b798c213cff897e7
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:30:06 +0530

    Update 04_questions.py

--- Level 1/04_questions.py
+++ Level 1/04_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a number from user and divide by the number by 6 and print the quotient. 
+Testcase: 
+Input: 45 → Output: 7 
+Input: 143 → Output: 23'''
+
 def divd(n):
     return n//6
 n = int(input("Enter a Integer: "))
