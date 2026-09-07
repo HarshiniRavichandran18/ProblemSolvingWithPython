96fdbeb090bf0764727ce67f162bfb2eddaa0b42
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:36:57 +0530

    Update 28_questions.py
    Add functionality to check the sum of one's and hundred's digits.

--- Level 1/28_questions.py
+++ Level 1/28_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a three-digit number from user. If the sum of the one's digit and hundred's digit is 
+less than 10, then print "Success", otherwise print "Failure". 
+Testcase: 
+Input: 569 → Output: Failure 
+Input: 316 → Output: Success'''
+
 a = int(input("Enter a Number: "))
 def check(a):
     if (a % 10) + (a // 100) < 10:
