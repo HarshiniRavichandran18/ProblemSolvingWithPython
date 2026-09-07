cc7bc801324fbcf52bdbccd453093c18c5b9c4d7
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:35:20 +0530

    Update 08_questions.py

--- Level 1/08_questions.py
+++ Level 1/08_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and print the one's digit. 
+Testcase: 
+Input: 456 → Output: 6 
+Input: 569 → Output: 9'''
+
 a = int(input("Enter a Integer: "))
 def ones(a):
     return a % 10
