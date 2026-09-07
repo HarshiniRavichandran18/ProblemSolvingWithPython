00a028e0ab00e759845cafbfbcff033a6ef31d18
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:43:07 +0530

    Update 28_questions.py

--- Level 2/28_questions.py
+++ Level 2/28_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a program to get two numbers from the user and print the LCM of those 
+numbers. 
+Testcase: 
+Input: 12, 18 → Output: 36 
+Input: 15, 20 → Output: 60'''
+
 a = int(input("Enter first Number: "))
 b = int(input("Enter second Number: "))
 if a > b:
