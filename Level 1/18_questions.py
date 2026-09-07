463f6fd74c9a54c48e7b4b263c4722957f0e7c3d
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:52:06 +0530

    Update 18_questions.py
    Modify the tens digit of a two-digit number to 1.

--- Level 1/18_questions.py
+++ Level 1/18_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a two-digit number from user and make the ten's digit 1, then print it. 
+Testcase: 
+Input: 95 → Output: 15 
+Input: 82 → Output: 12 '''
+
 a = int(input("Enter a number: "))
 def tens(a):
     return 10 + (a % 10)
