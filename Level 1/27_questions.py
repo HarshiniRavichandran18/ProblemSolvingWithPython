eab9606721b7c2483ff7baed27bb53882f9726ce
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:36:28 +0530

    Update 27_questions.py
    Add logic to check if the sum of digits equals 10.

--- Level 1/27_questions.py
+++ Level 1/27_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a three-digit number from user. If the sum of the digits is 10 then print "Success", 
+otherwise print "Failure". 
+Testcase: 
+Input: 956 → Output: Failure 
+Input: 127 → Output: Success'''
+
 a = int(input("Enter a Number: "))
 def check(a):
     if (a // 100) + ((a // 10) % 10) + (a % 10) == 10:
