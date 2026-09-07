d96423a811cdf26b3547998b845bcf3c5651d51e
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:40:47 +0530

    Update 27_questions.py

--- Level 2/27_questions.py
+++ Level 2/27_questions.py
@@ -1,3 +1,8 @@
+'''Question: Write a program to print the total count of numbers less than 100000 whose 
+sum of digits is 14. 
+Testcase: 
+Output: 4995'''
+
 count = 0
 for i in range(1, 100000):
     a = i
