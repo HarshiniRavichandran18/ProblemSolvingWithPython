89f590e197d58159b53a2e794be824099f68a516
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:33:49 +0530

    Update 16_questions.py

--- Level 2/16_questions.py
+++ Level 2/16_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a program to get a number from the user and print whether that number 
+is prime or not. 
+Testcase: 
+Input: 31 → Output: Prime 
+Input: 27 → Output: Not Prime'''
+
 a = int(input("Enter a Number: "))
 count = 0
 for i in range(1, a + 1):
