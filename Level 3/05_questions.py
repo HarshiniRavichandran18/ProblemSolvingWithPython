5e9f43e27249c877597550ba08124213c660c489
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:46:26 +0530

    Update 05_questions.py

--- Level 3/05_questions.py
+++ Level 3/05_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a number from user and count the number of zeros in that number. 
+Testcase: 
+Input: 100 → Output: 2 
+Input: 1060030 → Output: 4'''
+
 def check(a):
     count = 0
     while a > 0:
