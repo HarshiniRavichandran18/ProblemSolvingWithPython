ca4087b6e57a1e2fb0dc65dd82da6d8eb0667758
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:36:18 +0530

    Update 10_questions.py
    Added a function to extract and print the ten's digit from a three-digit number input.

--- Level 1/10_questions.py
+++ Level 1/10_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and print the ten's digit. 
+Testcase: 
+Input: 456 → Output: 5 
+Input: 569 → Output: 6'''
+
 a = int(input("Enter a Integer: "))
 def ones(a):
     return (a // 10) % 10
