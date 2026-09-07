08dced03890ebcd5e2d0db0fc2fba1f70e60989a
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:33:57 +0530

    Update 06_questions.py

--- Level 1/06_questions.py
+++ Level 1/06_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a two-digit number from user and print the one's digit. 
+Testcase: 
+Input: 45 → Output: 5 
+Input: 56 → Output: 6'''
+
 a = int(input("Enter a Integer: "))
 def ones(a):
     return a % 10
