a0b20dac40eb1458c1fa19d9777976abff568a68
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:29:09 +0530

    Update 03_questions.py

--- Level 1/03_questions.py
+++ Level 1/03_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a number from user and multiply 3 to that number and print the result. 
+Testcase: 
+Input: 45 → Output: 135 
+Input: 1200 → Output: 3600'''
+
 def mul(n):
     return n*3
 n = int(input("Enter a Integer: "))
