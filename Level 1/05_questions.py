1ea37d81de876d553eb4b03466a8b4640e9c998a
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:30:41 +0530

    Update 05_questions.py
    Updated docstring for clarity and fixed formatting.

--- Level 1/05_questions.py
+++ Level 1/05_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a number from user and divide by the number by 8 and print the remainder. 
+Testcase: 
+Input: 45 → Output: 5 
+Input: 143 → Output: 7'''
+
 def divd(n):
     return n%8
 n = int(input("Enter a Integer: "))
