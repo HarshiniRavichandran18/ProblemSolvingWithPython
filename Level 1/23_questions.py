d2dd76f04976c424a819db933527c95e70e0882d
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:22:33 +0530

    Update 23_questions.py
    Implement subtraction of 5 based on digit sum parity without using 'if'.

--- Level 1/23_questions.py
+++ Level 1/23_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a two-digit number from user and subtract 5 from that number if the sum of the 
+digits of the number is odd, then print the result. Do not use "if". 
+Testcase: 
+Input: 95 → Output: 95 
+Input: 72 → Output: 67 '''
+
 a = int(input("Enter a Number: "))
 
 def sum_odd(a):
