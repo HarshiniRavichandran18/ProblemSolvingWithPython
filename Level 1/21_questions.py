42c0eb41aecaeb5328b343a40496fdb8a4bf801f
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:20:18 +0530

    Update 21_questions.py

--- Level 1/21_questions.py
+++ Level 1/21_questions.py
@@ -1,3 +1,9 @@
+''Question: Get a number from user and subtract 5 from that number if the number is odd, then 
+print the result. Do not use "if". 
+Testcase: 
+Input: 695 → Output: 690 
+Input: 182 → Output: 182 '''
+
 a = int(input("Enter a Number: "))
 def odd(a):
     return a - (a % 2) * 5
