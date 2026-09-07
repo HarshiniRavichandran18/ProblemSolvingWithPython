64a8ccd6e61500670aab8683e0184255520d8a85
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:32:48 +0530

    Update 14_questions.py

--- Level 2/14_questions.py
+++ Level 2/14_questions.py
@@ -1,3 +1,10 @@
+'''Question: Write a program to get a number from the user and interchange the first and 
+last digits, then print the result. 
+Testcase: 
+Input: 123456 → Output: 623451 
+Input: 76895439 → Output: 96895437 
+Input: 675 → Output: 576 '''
+
 a = int(input("Enter a Number: "))
 last = a % 10
 temp = a
