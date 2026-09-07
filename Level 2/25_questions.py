ed7326588bf8bd4b0d8de4ac386309d7f5eb8645
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:39:55 +0530

    Update 25_questions.py

--- Level 2/25_questions.py
+++ Level 2/25_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a program to get a number from the user and print the total number of 
+single-digit prime numbers in the number.
+Testcase: 
+Input: 163496481 → Output: 1 
+Input: 364925 → Output: 3'''
+
 a = int(input("Enter a Number: "))
 count = 0
 while a > 0:
