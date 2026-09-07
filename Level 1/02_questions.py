e8acd502fe7bcf71f5053dbba055fc818bbd81e0
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:24:51 +0530

    Update 02_questions.py
    Updated the docstring to clarify the input and output examples.

--- Level 1/02_questions.py
+++ Level 1/02_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a number from user and subtract 5 to that number and print the result. 
+Testcase: 
+Input: 45 → Output: 40 
+Input: 56789 → Output: 56784'''
+
 def sub(n):
     return n-5
 n = int(input("Enter a Integer: "))
