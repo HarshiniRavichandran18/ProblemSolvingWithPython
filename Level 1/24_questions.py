d0cd4256310c48ba1167bcc829e55a95e322818e
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:23:44 +0530

    Update 24_questions.py
    Implement logic to subtract 5 from a three-digit number if the one's and hundred's digits are the same without using 'if'.

--- Level 1/24_questions.py
+++ Level 1/24_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a three-digit number from user and subtract 5 from that number if one's digit and 
+hundred's digit are the same, then print the result. Do not use "if". 
+Testcase: 
+Input: 595 → Output: 590 
+Input: 372 → Output: 372 '''
+
 a = int(input("Enter a Number: "))
 def same(a):
     return a - ((a // 100 == a % 10) * 5)
