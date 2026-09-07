af0d8431314ec50a770a3d47d91bf07d2dccbba2
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:28:00 +0530

    Update 09_questions.py

--- Level 2/09_questions.py
+++ Level 2/09_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a loop program to print the sum of two-digit numbers whose one's digit 
+is 5. 
+ 
+Testcase: 
+Output: 495 '''
+
 sum = 0
 for i in range(10, 100):
     if i % 10 == 5:
