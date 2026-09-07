2839a8f1d4aa72e3404039e1d2faf0f7868fe638
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:18:04 +0530

    Update 32_questions.py
    Add logic to check the sum of two numbers and print accordingly.

--- Level 1/32_questions.py
+++ Level 1/32_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get two 2-digit numbers from user. If the sum of the numbers is less than 100, then 
+print the sum, otherwise print the difference. 
+Testcase: 
+Input: 56, 78 → Output: 22 
+Input: 14, 65 → Output: 79'''
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 def check(a, b):
