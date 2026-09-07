b3e32bf37cefc763deede8e64103c23178e155df
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:50:38 +0530

    Update 07_questions.py

--- Level 3/07_questions.py
+++ Level 3/07_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get two numbers from user and compare them. If they are the same, print 
+Same; otherwise print Not Same. 
+Testcase: 
+Input: 123, 123 → Output: Same 
+Input: 56789, 12345 → Output: Not Same '''
+
 def check(a, b):
     if a == b:
         return "Same"
