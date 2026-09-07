c96b49295bffeaaca090a862bf7611fd5ee2298c
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:51:47 +0530

    Update 09_questions.py

--- Level 3/09_questions.py
+++ Level 3/09_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a two-digit number from user and swap the digits. 
+Testcase: 
+Input: 34 → Output: 43 
+Input: 56 → Output: 65 '''
+
 def check(a):
     return (a % 10) * 10 + (a // 10)
 a = int(input("Enter a Number: "))
