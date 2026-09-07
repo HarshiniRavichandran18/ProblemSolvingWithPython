23500a84a48aadf74a361711b4f922aace3d4bd6
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:14:33 +0530

    Update 29_questions.py
    Update the condition to check if the sum of the ten's and hundred's digits is greater than 10.

--- Level 1/29_questions.py
+++ Level 1/29_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a four-digit number from user. If the sum of the ten's digit and hundred's digit is 
+greater than 10, then print "Success", otherwise print "Failure". 
+Testcase: 
+Input: 7529 → Output: Failure 
+Input: 9386 → Output: Success '''
+
 a = int(input("Enter a Number: "))
 def check(a):
     if (a % 10) + (a // 100) < 10:
