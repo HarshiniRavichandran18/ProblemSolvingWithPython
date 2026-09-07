1ad9ae62648be5a12de00f6b110aec1fb830796d
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:00:49 +0530

    Update 19_questions.py
    Modify the one's digit of a three-digit number to 2 and print the result.

--- Level 1/19_questions.py
+++ Level 1/19_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and make the one's digit as 2, then print it. 
+Testcase: 
+Input: 695 → Output: 692 
+Input: 182 → Output: 182'''
+
 a = int(input("Enter a Number: "))
 def ones(a):
     return (a // 10) * 10 + 2
