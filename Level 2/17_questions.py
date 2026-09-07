93bb94f1a1533476408168c8d3e3428ef3fef8b8
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:34:26 +0530

    Update 17_questions.py

--- Level 2/17_questions.py
+++ Level 2/17_questions.py
@@ -1,3 +1,10 @@
+'''Question: Write a program to get a number from the user, print whether that number is 
+prime, and check whether the sum of its digits is equal to 14. 
+Testcase: 
+Input: 59 → Output: Prime & Sum of Digits is 14 
+Input: 77 → Output: Not Prime but sum of digits is 14 
+Input: 13 → Output: Prime, but sum of Digits is not 14 '''
+
 a = int(input("Enter a Number: "))
 count = 0
 for i in range(1, a + 1):
