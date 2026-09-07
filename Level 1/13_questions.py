f1766a7c75f3834f76d65c6bae0c1b305d7bbbcc
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:37:58 +0530

    Update 13_questions.py
    Added a function to reverse a two-digit number input by the user.

--- Level 1/13_questions.py
+++ Level 1/13_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a two-digit number from user and print the reverse of the number. 
+Testcase: 
+Input: 56 → Output: 65 
+Input: 59 → Output: 95'''
+
 a = int(input("Enter a Number: "))
 def rev(a):
     return (a % 10) * 10 + (a // 10)
