a8cae9dd3bc13f916c1dcc8130f81bb0ef46c97e
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:31:00 +0530

    Update 11_questions.py

--- Level 2/11_questions.py
+++ Level 2/11_questions.py
@@ -1,3 +1,11 @@
+'''Question: Write a program to get a number from the user and print the total number of 
+digits in that number. 
+ 
+Testcase: 
+Input: 123456 → Output: 6 
+Input: 76895439 → Output: 8 
+Input: 675 → Output: 3 '''
+
 a = int(input("Enter a number: "))
 count = 0
 while a > 0:
