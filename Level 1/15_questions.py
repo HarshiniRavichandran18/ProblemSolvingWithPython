3a1322cc62be512af168dc83c4fc9bbc1295d40f
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:50:50 +0530

    Update 15_questions.py
    Implemented a function to reverse the first two digits of a four-digit number.

--- Level 1/15_questions.py
+++ Level 1/15_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a four-digit number from user and only reverse the first two digits of the number, 
+then print the number. 
+Testcase: 
+Input: 9561 → Output: 9516 
+Input: 3859 → Output: 3895'''
+
 a = int(input("Enter a Number: "))
 def rev(a):
     return (a // 100) * 100 + (a % 10) * 10 + ((a // 10) % 10)
