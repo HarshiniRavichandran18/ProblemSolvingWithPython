c7352e8b2fbad6090fdb1ad5d7e8a42841500b06
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:30:40 +0530

    Update 10_questions.py

--- Level 2/10_questions.py
+++ Level 2/10_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a loop program to print the sum of two-digit odd numbers whose ten's 
+digit is 7. 
+ 
+Testcase: 
+Output: 375'''
+
 sum = 0
 for i in range(10, 100):
     if i % 2 != 0 and i // 10 == 7:
