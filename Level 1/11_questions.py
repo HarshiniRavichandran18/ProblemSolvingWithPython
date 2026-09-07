ba33f515065872a27d4be414ccba05cefee1ec83
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:36:54 +0530

    Update 11_questions.py
    Added functionality to sum the digits of a two-digit number input by the user.

--- Level 1/11_questions.py
+++ Level 1/11_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a two-digit number from user and print sum the digits. 
+Testcase: 
+Input: 56 → Output: 11 
+Input: 69 → Output: 15 '''
+
 a = int(input("Enter a Integer: "))
 def ones(a):
     return (a // 10) + (a % 10)
