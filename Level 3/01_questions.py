9c0ed9c60dc23f957cb9ef6015053d47b68650dd
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:44:44 +0530

    Update 01_questions.py

--- Level 3/01_questions.py
+++ Level 3/01_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a number from user and add 2 to that number and print the result. Write 
+your code inside the function. 
+Testcase: 
+Input: 45 → Output: 47 
+Input: 56789 → Output: 56791'''
+
 def check(a):
     return a + 2
 a = int(input("Enter a Number: "))
