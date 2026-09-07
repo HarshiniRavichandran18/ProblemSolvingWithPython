a88c85961bdbd30b3d55f7d468b27d155d7c59bd
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:37:31 +0530

    Update 12_questions.py
    Added a function to calculate the sum of digits of a three-digit number.

--- Level 1/12_questions.py
+++ Level 1/12_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and print sum the digits. 
+Testcase: 
+Input: 562 → Output: 13 
+Input: 469 → Output: 19 '''
+
 a = int(input("Enter a Integer: "))
 def ones(a):
     return (a // 100) + ((a // 10) % 10) + (a % 10)
