e6dc210bb85ef80000c8b4b32d63048052de2c99
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:17:14 +0530

    Update 31_questions.py
    Implement digit sum logic for three-digit numbers.

--- Level 1/31_questions.py
+++ Level 1/31_questions.py
@@ -1,3 +1,10 @@
+'''Question: Get a three-digit number from user. If the sum of the digits is less than 10, then print 
+the sum, otherwise add the digits of the sum and continue until the result is a single digit. 
+Testcase: 
+Input: 123 → Output: 6 
+Input: 149 → Output: 5 
+Input: 991 → Output: 1'''
+
 a = int(input("Enter a Number: "))
 def sum_digit(a):
     s = (a // 100) + ((a // 10) % 10) + (a % 10)
