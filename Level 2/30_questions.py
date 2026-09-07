9fc50fda8b9db26828bcfe36983acc29551197e4
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:43:57 +0530

    Update 30_questions.py

--- Level 2/30_questions.py
+++ Level 2/30_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a program to get two numbers from the user and print the HCF of those 
+numbers. 
+Testcase: 
+Input: 12, 18 → Output: 6 
+Input: 24, 36 → Output: 12'''
+
 a = int(input("Enter first Number: "))
 b = int(input("Enter second Number: "))
 if a > b:
