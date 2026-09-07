d98760f848ef6dc641d3e714fface1256693460e
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:36:59 +0530

    Update 21_questions.py

--- Level 2/21_questions.py
+++ Level 2/21_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a program to get a number from the user and print the total number of 
+digits that are odd. 
+Testcase: 
+Input: 12345678 → Output: 4 
+Input: 987531 → Output: 5'''
+
 a = int(input("Enter a Number: "))
 count = 0
 while a > 0:
