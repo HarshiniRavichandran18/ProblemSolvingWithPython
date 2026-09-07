89309a44ab98115b4a87fe3081c19fa07cadcbc9
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:24:09 +0530

    Update 25_questions.py
    Implement logic to subtract 5 from a four-digit number based on digit comparison without using 'if'.

--- Level 1/25_questions.py
+++ Level 1/25_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a four-digit number from user and subtract 5 from that number if ten's digit 
+position and hundred's digit position are the same, then print the result. Do not use "if". 
+Testcase: 
+Input: 7595 → Output: 7595 
+Input: 3772 → Output: 3767'''
+
 a = int(input("Enter a Number: "))
 def same(a):
     return a - ((((a // 10) % 10) == ((a // 100) % 10)) * 5)
