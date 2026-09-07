d8738dd43c56f0172b2397eb0d34fcde20ad209d
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:35:48 +0530

    Update 09_questions.py

--- Level 1/09_questions.py
+++ Level 1/09_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and print the hundred's digit. 
+Testcase: 
+Input: 456 → Output: 4 
+Input: 569 → Output: 5'''
+
 a = int(input("Enter a Integer: "))
 def tens(a):
     return a // 100
