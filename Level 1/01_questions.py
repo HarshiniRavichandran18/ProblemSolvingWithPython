b0c8cf70c8448ab159b8257a6e5be5c839679871
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:23:59 +0530

    Update 01_questions.py
    Added a function to add 2 to user input and print the result.

--- Level 1/01_questions.py
+++ Level 1/01_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a number from user and add 2 to that number and print the result. 
+Testcase: 
+Input: 45 → Output: 47 
+Input: 56789 → Output: 56791'''
+
 def add(n):
     return n+2
 n = int(input("Enter a Integer: "))
