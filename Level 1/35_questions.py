59d6947eed0c466e98ed546e2b7ad4c60ca96ef8
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:21:43 +0530

    Update 35_questions.py

--- Level 1/35_questions.py
+++ Level 1/35_questions.py
@@ -1,3 +1,10 @@
+'''Question: Get two 3-digit numbers from user. Add the one's and hundred's digits of both 
+numbers. Print the sum of all the digits of the number whose sum of one's and hundred's digits 
+is bigger. 
+Testcase: 
+Input: 856, 978 → Output: 24 
+Input: 128, 365 → Output: 11'''
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 def check(a, b):
