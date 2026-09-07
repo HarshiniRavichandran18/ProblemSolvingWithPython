7c84794579cf009e4856349cb17b52bde6b874c7
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:19:34 +0530

    Update 20_questions.py
    Modified the function to set the ten's digit to 0 in a three-digit number.

--- Level 1/20_questions.py
+++ Level 1/20_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and make the ten's digit as 0, then print it. 
+Testcase: 
+Input: 695 → Output: 605 
+Input: 182 → Output: 102'''
+
 a = int(input("Enter a Number: "))
 def ones(a):
     return (a // 100) * 100 + (a % 10)
