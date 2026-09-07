569678981a341bff52340584a7990e4ba4bf48cd
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:45:29 +0530

    Update 03_questions.py

--- Level 3/03_questions.py
+++ Level 3/03_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a number from user and check whether the sum of digits is 14, then print 
+the result. 
+Testcase: 
+Input: 59 → Output: Sum of Digits is 14 
+Input: 123 → Output: Sum of Digits is not 14'''
+
 def check(a):
     sum = 0
     while a > 0:
