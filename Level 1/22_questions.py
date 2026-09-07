b3018c532432268a6d98eabba42e38f86d35fc3f
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 11:21:15 +0530

    Update 22_questions.py
    Implement subtraction based on the ten's position digit being odd without using 'if'.

--- Level 1/22_questions.py
+++ Level 1/22_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a number from user and subtract 5 from that number if the number's ten's position 
+digit is odd, then print the result. Do not use "if". 
+Testcase: 
+Input: 685 → Output: 685 
+Input: 89172 → Output: 89167'''
+
 a = int(input("Enter a Number: "))
 def odd(a):
     return a - ((a // 10) % 2) * 5
