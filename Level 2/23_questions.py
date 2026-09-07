d34321305b853429b22aa19e21acbd4abe9579d1
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:38:11 +0530

    Update 23_questions.py

--- Level 2/23_questions.py
+++ Level 2/23_questions.py
@@ -1,3 +1,9 @@
+'''Question: Write a program to get a number from the user and print the total number of 
+single-digit perfect square numbers in the number. 
+Testcase: 
+Input: 123456789 → Output: 3 
+Input: 987531 → Output: 2'''
+
 a = int(input("Enter a Number: "))
 count = 0
 while a > 0:
