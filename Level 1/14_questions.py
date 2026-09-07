27d6d0fbd36dee96eda8c593ecf88548f69f03da
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:50:17 +0530

    Update 14_questions.py
    Added a function to reverse a three-digit number input by the user.

--- Level 1/14_questions.py
+++ Level 1/14_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a three-digit number from user and print the reverse of the number. 
+Testcase: 
+Input: 561 → Output: 165 
+Input: 859 → Output: 958'''
+
 a = int(input("Enter a Number: "))
 def rev(a):
     return ((a % 10) * 100) + ((a // 10) % 10) * 10 + (a // 100)
