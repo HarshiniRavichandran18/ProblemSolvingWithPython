7742c50299d4bf45a5f2e2c4f1434f4ac611fc6e
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:20:47 +0530

    Update 33_questions.py

--- Level 1/33_questions.py
+++ Level 1/33_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get two 2-digit numbers from user. Print the sum of digits of the biggest number. 
+Testcase: 
+Input: 56, 78 → Output: 15 
+Input: 14, 65 → Output: 11'''
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 def check(a, b):
