729dad1dd8a9b9a54f8d028512074ef845266a50
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:51:40 +0530

    Update 17_questions.py
    Added functionality to replace the one's digit of a two-digit number with 0.

--- Level 1/17_questions.py
+++ Level 1/17_questions.py
@@ -1,3 +1,8 @@
+'''Question: Get a two-digit number from user and make the one's digit as 0, then print it. 
+Testcase: 
+Input: 95 → Output: 90 
+Input: 18 → Output: 10 '''
+
 a = int(input("Enter a Number: "))
 def ones(a):
     return (a // 10)* 10
