8ae162620aa46376480d313588f5cc24cba45ddc
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:31:48 +0530

    Update 13-questions.py

--- Level 2/13-questions.py
+++ Level 2/13-questions.py
@@ -1,3 +1,10 @@
+'''Question: Write a program to get a number from the user and print the reverse of that 
+number. 
+Testcase: 
+Input: 123456 → Output: 654321 
+Input: 76895439 → Output: 93459867 
+Input: 675 → Output: 576 '''
+
 a = int(input("Enter a Number: "))
 rev = 0
 while a > 0:
