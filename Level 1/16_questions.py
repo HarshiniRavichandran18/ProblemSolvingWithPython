d5a05d97300b7c8c64651851a265390e044f5118
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 10:51:16 +0530

    Update 16_questions.py
    Reversed the last two digits of a four-digit number input by the user.

--- Level 1/16_questions.py
+++ Level 1/16_questions.py
@@ -1,3 +1,9 @@
+'''Question: Get a four-digit number from user and only reverse the last two digits of the number, 
+then print the number. 
+Testcase: 
+Input: 9561 → Output: 5961 
+Input: 3859 → Output: 8359'''
+
 a = int(input("Enter a Number: "))
 def rev(a):
     return (a // 100) % 10 * 1000 + (a // 1000) * 100 + (a % 100)
