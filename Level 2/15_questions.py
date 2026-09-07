ca58243f97ba32bd58f5a6bbe6458e8a4c5ffb9b
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-09-07 14:33:08 +0530

    Update 15_questions.py

--- Level 2/15_questions.py
+++ Level 2/15_questions.py
@@ -1,3 +1,12 @@
+'''Question: Write a program to get a number from the user. If the first digit is even, print 
+the same number. If the first digit is odd, subtract 1 from the first digit and print the 
+number. 
+Testcase: 
+Input: 123456 → Output: 023456 
+Input: 96895439 → Output: 86895439 
+Input: 675 → Output: 675 
+Input: 575 → Output: 475 '''
+
 a = int(input("Enter a Number: "))
 def check(a):
     first = a
